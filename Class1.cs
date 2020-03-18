67c97332c11275b84747a8072957be4e53564dff
Osawerengeka <mr.georgegevork@gmail.com>
2020-03-19 00:22:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submodule2
+{
+    class stringB
+    {
+        public string merge(string[] s)
+        {
+            string res = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                res += " " + s[i];
+
+            }
+            return res;
+        
+        }
+
+    }
+}
